4ccd575eae06ed5ff469938fd7cfec3caf08f345
NoMercy17 <antonio.stiube@gmail.com>
2026-06-24 17:22:10 +0300

    created a VPC (virtual Private Cloud) on aws

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -1,13 +1,26 @@
 package com.pm.stack;
 
+import com.amazonaws.services.route53.model.VPC;
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.Vpc;
 
 public class LocalStack extends Stack {
 
-    // nothing in here yet
+    private final Vpc vpc;
+
     public LocalStack(final App scope, final String id, final StackProps props){
         super(scope, id, props);
+        this.vpc = createVpc();
     }
+
+    private Vpc createVpc(){
+        return Vpc.Builder
+                .create(this, "PatientManagementVPC")
+                .vpcName("PatientManagementVPC")
+                .maxAzs(2) // vpc will be available in 2 different zones throughout the world
+                .build();
+    }
+
 
     public static void main(final String[] args){
         // create a new aws cdk app, and where the output should be
