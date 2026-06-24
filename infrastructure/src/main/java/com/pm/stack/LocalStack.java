8fd12f8c8bd6d9526f164b301ef710aca2fff004
NoMercy17 <antonio.stiube@gmail.com>
2026-06-24 17:47:25 +0300

    created the rds space for our postgre databases. Create the function for db creation as well as the equivalent to our db names/id. Created the health checks for them

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -2,7 +2,12 @@ package com.pm.stack;
 
 import com.amazonaws.services.route53.model.VPC;
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.InstanceClass;
+import software.amazon.awscdk.services.ec2.InstanceSize;
+import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.rds.*;
+import software.amazon.awscdk.services.route53.CfnHealthCheck;
 
 public class LocalStack extends Stack {
 
@@ -11,6 +16,12 @@ public class LocalStack extends Stack {
     public LocalStack(final App scope, final String id, final StackProps props){
         super(scope, id, props);
         this.vpc = createVpc();
+
+        DatabaseInstance authServiceDb = createDatabase("AuthServiceDB", "auth-service-db");
+        DatabaseInstance patientServiceDb = createDatabase("PatientServiceDb", "patient-service-db");
+
+        CfnHealthCheck authDbHealthCheck = createDbHealthCheck(authServiceDb, "AuthServiceDBHealthCheck");
+        CfnHealthCheck patientDbHealthCheck = createDbHealthCheck(patientServiceDb, "PatientServiceDBHealthCheck");
     }
 
     private Vpc createVpc(){
@@ -21,6 +32,36 @@ public class LocalStack extends Stack {
                 .build();
     }
 
+    private DatabaseInstance createDatabase(String id, String dbName){
+        return DatabaseInstance.Builder
+                .create(this, id)
+                .engine(DatabaseInstanceEngine.postgres(
+                        PostgresInstanceEngineProps.builder()
+                                .version(PostgresEngineVersion.VER_17_2)
+                                .build()))
+                .vpc(vpc) // database connected to the vpc
+                .instanceType(InstanceType.of
+                        (InstanceClass.BURSTABLE2, InstanceSize.MICRO))
+                        // CPU, computer power, store, etc... that we want this db to run on
+                .allocatedStorage(20) // the amount of storage to this db
+                .credentials(Credentials.fromGeneratedSecret("admin_user"))
+                .databaseName(dbName)
+                .removalPolicy(RemovalPolicy.DESTROY) /// when we remove the stack we remove the db storage(remove the lingering data)
+                .build();
+    }
+
+    // give the health status of the database
+    private CfnHealthCheck createDbHealthCheck(DatabaseInstance db, String id){
+        return CfnHealthCheck.Builder.create(this, id)
+                .healthCheckConfig(CfnHealthCheck.HealthCheckConfigProperty.builder()
+                        .type("TCP") // type of the health check
+                        .port(Token.asNumber(db.getDbInstanceEndpointPort())) // going to get the port that our db is running on
+                        .ipAddress(db.getDbInstanceEndpointAddress())
+                        .requestInterval(20) // check at every 20 sec
+                        .failureThreshold(3) // try 3 times before reports failure
+                        .build())
+                .build();
+    }
 
     public static void main(final String[] args){
         // create a new aws cdk app, and where the output should be
