5361a7686c5154f4a70ec65b200ab40fea8ddd87
NoMercy17 <antonio.stiube@gmail.com>
2026-06-30 12:46:31 +0300

    created the ECS clusters with the ECS services for our microservies, similar to the docker environment

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -3,12 +3,17 @@ package com.pm.stack;
 import software.amazon.awscdk.*;
 import software.amazon.awscdk.services.ec2.*;
 import software.amazon.awscdk.services.ec2.InstanceType;
-import software.amazon.awscdk.services.ecs.CloudMapNamespaceOptions;
-import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.*;
+import software.amazon.awscdk.services.ecs.Protocol;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.amazon.awscdk.services.logs.RetentionDays;
 import software.amazon.awscdk.services.msk.CfnCluster;
 import software.amazon.awscdk.services.rds.*;
 import software.amazon.awscdk.services.route53.CfnHealthCheck;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LocalStack extends Stack {
@@ -29,6 +34,34 @@ public class LocalStack extends Stack {
         CfnCluster mskCluster = createMSKCluster();
 
         this.ecsCluster = createEcsCluster();
+
+        // Services creation
+        FargateService authService = createFargateService("AuthService", "auth-service",
+                List.of(4005), authServiceDb, Map.of("JWT_SECRET","7qUQ8QhGeDMj8Fd29EuYuUqpuxduEpFs07nySU2kS0E"));
+
+        // tells the CDK and cloud formation template that authService has a dependency to the db and health check
+        authService.getNode().addDependency(authDbHealthCheck);
+        authService.getNode().addDependency(authServiceDb);
+
+        FargateService billingService = createFargateService("BillingService", "billing-service",
+                List.of(4001, 9001), null, null);
+
+        FargateService analytics = createFargateService("AnalyticsService", "analytics-service",
+                List.of(4002), null, null);
+
+        // kafka cluster needs to be running before we start the analytics
+        analytics.getNode().addDependency(mskCluster);
+
+        FargateService patientService = createFargateService("PatientService", "patient-service",
+                List.of(4000), patientServiceDb, Map.of(
+                        "BILLING_SERVICE_ADDRESS", "host.docker.internal",
+                        "BILLING_SERVICE_GRPC_PORT", "9001"
+                ));
+
+        patientService.getNode().addDependency(patientServiceDb);
+        patientService.getNode().addDependency(patientDbHealthCheck);
+        patientService.getNode().addDependency(billingService);
+        patientService.getNode().addDependency(mskCluster);
     }
 
     private Vpc createVpc(){
@@ -83,6 +116,76 @@ public class LocalStack extends Stack {
                                 .brokerAzDistribution("DEFAULT").build())// how broker get distributed
                 .build();
     }
+
+    // type of ECS service, easy to start/stop and scale ECS tasks
+    private FargateService createFargateService(String id, String imageName,
+                                                List<Integer> ports,
+                                                DatabaseInstance db,
+                                                Map<String, String> additionEnvVars){
+        // task definition = blueprint for container
+        FargateTaskDefinition taskDefinition = FargateTaskDefinition.Builder
+                .create(this, id + "Task")
+                .cpu(256)
+                .memoryLimitMiB(512) // MB
+                .build();
+
+
+        ContainerDefinitionOptions.Builder containerOptions = ContainerDefinitionOptions.builder()
+                .image(ContainerImage.fromRegistry(imageName))
+                .portMappings(ports.stream()
+                        .map(port -> PortMapping.builder()
+                                .containerPort(port)
+                                .hostPort(port) // the port that gets exposed for other services to access it
+                                .protocol(Protocol.TCP)
+                                .build())
+                        .toList())
+                .logging(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                .logGroup(LogGroup.Builder.create(this, id + "LogGroup").logGroupName("/ecs" + imageName)
+                                        .removalPolicy(RemovalPolicy.DESTROY)
+                                        .retention(RetentionDays.ONE_DAY)
+                                        .build()) // we want to group all the logs for a given container/service into their own group with the same name
+                                .streamPrefix(imageName)
+                        .build())); // where our logging will go into our container
+
+
+        Map<String, String> envVars = new HashMap<>();
+
+        envVars.put("SPRING_KAFKA_BOOTSTRAP_SERVERS", "localhost.localstack.cloud:4510, localhost.localstack.cloud:4511, localhost.localstack.cloud:4512");
+
+
+        if(additionEnvVars != null){
+            envVars.putAll(additionEnvVars);
+        }
+
+        // connection for the server to connect to the db that has been passed in
+        if(db != null){
+            envVars.put("SPRING_DATASOURCE_URL", "jdbc:postgresql://%s:%s/%s-db".formatted(
+                    db.getDbInstanceEndpointAddress(), db.getDbInstanceEndpointPort(), imageName
+            ));
+
+            envVars.put("SPRING_DATASOURCE_USERNAME", "admin_user");
+            envVars.put("SPRING_DATASOURCE_PASSWORD", db.getSecret().secretValueFromJson("password").toString());
+
+            envVars.put("SPRING_JPA_HIBERNATE_DDL_AUTO", "update");
+            envVars.put("SPRING_SQL_INIT_MODE", "always");
+            envVars.put("SPRING_DATASOURCE_HIKARI_INITIALIZATION_FAIL_TIMEOUT", "60000");
+        }
+
+
+        containerOptions.environment(envVars).build();
+
+        // adds a new container to our task definition with those options, that we build it for passing
+        taskDefinition.addContainer(imageName + "Container", containerOptions.build());
+
+        return FargateService.Builder.create(this, id)
+                .cluster(ecsCluster)
+                .taskDefinition(taskDefinition) // will use this task definition to start a container
+                .assignPublicIp(false) // closed to the internet
+                .serviceName(imageName)
+                .build();
+    }
+
+
 
     // other microservices can find the auth service by: auth-service.patient-management.local
     private Cluster createEcsCluster(){
